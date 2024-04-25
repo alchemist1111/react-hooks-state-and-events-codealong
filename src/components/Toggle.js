f4ee1324f297f38e9a3c1c832ab69d36343d900a
alchemist1111 <emoyoeugine@gmail.com>
2024-04-25 18:12:26 +0300

    adds content in the Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(true);
+  function handleClick() {
+    setIsOn((isOn) => !isOn)
+    console.log(setIsOn);
+  }
+  const color = isOn ? "purple" : "blue";
+  return(
+  <button className="btn" style={{background: color}} onClick={handleClick}>
+    {isOn ? "ON" : "OFF"}</button>
+    ); 
 }
 
 export default Toggle;
